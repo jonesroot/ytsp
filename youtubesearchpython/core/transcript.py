d26726682c3d618f12ce933cf429860a93d49840
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-03-07 16:14:24 +0700

    Remove import "'*"

--- youtubesearchpython/core/transcript.py
+++ youtubesearchpython/core/transcript.py
@@ -3,7 +3,7 @@ import json
 from typing import Union, List
 from urllib.parse import urlencode
 
-from youtubesearchpython.core.constants import *
+from youtubesearchpython.core.constants import searchKey, requestPayload
 from youtubesearchpython.core.requests import RequestCore
 from youtubesearchpython.core.componenthandler import getValue, getVideoId
 
