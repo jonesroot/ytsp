53fe0217290c6dd2adbc34f95ec2c7b00cf4f5da
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-03-07 16:54:02 +0700

    try fixing requests method

--- youtubesearchpython/core/requests.py
+++ youtubesearchpython/core/requests.py
@@ -3,37 +3,55 @@ import os
 
 from youtubesearchpython.core.constants import userAgent
 
+
 class RequestCore:
     def __init__(self):
         self.url = None
         self.data = None
         self.timeout = 2
-        self.proxy = {}
+        self.proxies = None
+        
         http_proxy = os.environ.get("HTTP_PROXY")
-        if http_proxy:
-            self.proxy["http://"] = http_proxy
         https_proxy = os.environ.get("HTTPS_PROXY")
-        if https_proxy:
-            self.proxy["https://"] = https_proxy
+
+        if http_proxy or https_proxy:
+            self.proxies = {
+                "http://": http_proxy,
+                "https://": https_proxy
+            }
 
     def syncPostRequest(self) -> httpx.Response:
-        return httpx.post(
-            self.url,
-            headers={"User-Agent": userAgent},
-            json=self.data,
-            timeout=self.timeout,
-            proxies=self.proxy
-        )
+        with httpx.Client(proxies=self.proxies) as client:
+            return client.post(
+                self.url,
+                headers={"User-Agent": userAgent},
+                json=self.data,
+                timeout=self.timeout
+            )
 
     async def asyncPostRequest(self) -> httpx.Response:
-        async with httpx.AsyncClient(proxies=self.proxy) as client:
-            r = await client.post(self.url, headers={"User-Agent": userAgent}, json=self.data, timeout=self.timeout)
-            return r
+        async with httpx.AsyncClient(proxies=self.proxies) as client:
+            return await client.post(
+                self.url,
+                headers={"User-Agent": userAgent},
+                json=self.data,
+                timeout=self.timeout
+            )
 
     def syncGetRequest(self) -> httpx.Response:
-        return httpx.get(self.url, headers={"User-Agent": userAgent}, timeout=self.timeout, cookies={'CONSENT': 'YES+1'}, proxies=self.proxy)
+        with httpx.Client(proxies=self.proxies) as client:
+            return client.get(
+                self.url,
+                headers={"User-Agent": userAgent},
+                timeout=self.timeout,
+                cookies={'CONSENT': 'YES+1'}
+            )
 
     async def asyncGetRequest(self) -> httpx.Response:
-        async with httpx.AsyncClient(proxies=self.proxy) as client:
-            r = await client.get(self.url, headers={"User-Agent": userAgent}, timeout=self.timeout, cookies={'CONSENT': 'YES+1'})
-            return r
+        async with httpx.AsyncClient(proxies=self.proxies) as client:
+            return await client.get(
+                self.url,
+                headers={"User-Agent": userAgent},
+                timeout=self.timeout,
+                cookies={'CONSENT': 'YES+1'}
+            )
