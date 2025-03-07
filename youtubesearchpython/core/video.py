e1f5696ac5d9f0d2217b8716d4601f888a86a365
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-03-07 18:47:59 +0700

    update youtubesearchpython/core/video.py

--- youtubesearchpython/core/video.py
+++ youtubesearchpython/core/video.py
@@ -102,7 +102,7 @@ class VideoCore(RequestCore):
         if response.status_code == 200:
             self.post_request_processing()
         else:
-            raise f"ERROR status code: {response.status_code}"
+            raise Exception(f"ERROR status code: {response.status_code}")
 
     def prepare_html_request(self):
         self.url = (
