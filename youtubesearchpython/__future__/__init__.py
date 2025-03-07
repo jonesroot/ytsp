c390db261f85ed7aef8d05a0b499589da8324b62
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-03-07 15:45:24 +0700

    Remove import "*"

--- youtubesearchpython/__future__/__init__.py
+++ youtubesearchpython/__future__/__init__.py
@@ -1,11 +1,41 @@
 from youtubesearchpython.__future__.search import Search, VideosSearch, ChannelsSearch, PlaylistsSearch, CustomSearch, ChannelSearch
 from youtubesearchpython.__future__.extras import Video, Playlist, Suggestions, Hashtag, Comments, Transcript, Channel
 from youtubesearchpython.__future__.streamurlfetcher import StreamURLFetcher
-from youtubesearchpython.core.utils import *
-from youtubesearchpython.core.constants import *
+from youtubesearchpython.core.utils import playlist_from_channel_id
+from youtubesearchpython.core.constants import (
+    requestPayload,
+    userAgent,
+    videoElementKey,
+    channelElementKey,
+    playlistElementKey,
+    shelfElementKey,
+    itemSectionKey,
+    continuationItemKey,
+    playerResponseKey,
+    richItemKey,
+    hashtagElementKey,
+    hashtagBrowseKey,
+    hashtagVideosPath,
+    hashtagContinuationVideosPath,
+    searchKey,
+    contentPath,
+    fallbackContentPath,
+    continuationContentPath,
+    continuationKeyPath,
+    playlistInfoPath,
+    playlistVideosPath,
+    playlistPrimaryInfoKey,
+    playlistSecondaryInfoKey,
+    playlistVideoKey,
+    ResultMode, SearchMode,
+    VideoUploadDateFilter,
+    VideoDurationFilter,
+    VideoSortOrder,
+    ChannelRequestType
+)
 
 
-__title__        = 'youtube-search-python'
-__version__      = '1.6.2'
-__author__       = 'alexmercerind'
+__title__        = 'ytsp'
+__version__      = '2.0.0'
+__author__       = 'Lucifer'
 __license__      = 'MIT'
