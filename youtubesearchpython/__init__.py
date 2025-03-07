05183c1fa945bd12e0d82a3434e68b003d657107
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-03-07 16:34:23 +0700

    Remove import "*"

--- youtubesearchpython/__init__.py
+++ youtubesearchpython/__init__.py
@@ -1,16 +1,42 @@
 from youtubesearchpython.search import Search, VideosSearch, ChannelsSearch, PlaylistsSearch, CustomSearch, ChannelSearch
 from youtubesearchpython.extras import Video, Playlist, Suggestions, Hashtag, Comments, Transcript, Channel
 from youtubesearchpython.streamurlfetcher import StreamURLFetcher
-from youtubesearchpython.core.constants import *
-from youtubesearchpython.core.utils import *
-
-
-__title__        = 'youtube-search-python'
-__version__      = '1.6.2'
-__author__       = 'alexmercerind'
-__license__      = 'MIT'
-
-
-''' Deprecated. Present for legacy support. '''
+from youtubesearchpython.core.utils import playlist_from_channel_id
 from youtubesearchpython.legacy import SearchVideos, SearchPlaylists
-from youtubesearchpython.legacy import SearchVideos as searchYoutube
+from youtubesearchpython.core.constants import (
+    requestPayload,
+    userAgent,
+    videoElementKey,
+    channelElementKey,
+    playlistElementKey,
+    shelfElementKey,
+    itemSectionKey,
+    continuationItemKey,
+    playerResponseKey,
+    richItemKey,
+    hashtagElementKey,
+    hashtagBrowseKey,
+    hashtagVideosPath,
+    hashtagContinuationVideosPath,
+    searchKey,
+    contentPath,
+    fallbackContentPath,
+    continuationContentPath,
+    continuationKeyPath,
+    playlistInfoPath,
+    playlistVideosPath,
+    playlistPrimaryInfoKey,
+    playlistSecondaryInfoKey,
+    playlistVideoKey,
+    ResultMode, SearchMode,
+    VideoUploadDateFilter,
+    VideoDurationFilter,
+    VideoSortOrder,
+    ChannelRequestType
+)
+
+
+__title__        = 'ytsp'
+__version__      = '2.0.0'
+__author__       = 'Lucifer'
+__license__      = 'MIT'
